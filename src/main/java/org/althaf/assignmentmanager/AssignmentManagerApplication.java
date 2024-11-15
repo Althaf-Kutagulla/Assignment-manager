6f71c8a5df7b2933af2ab7f536dca5b9328b93d7
Althaf Kutagulla <kutagullaalthaf12@gmail.com>
2024-11-15 21:57:46 +0530

    intial commit

---
+++ src/main/java/org/althaf/assignmentmanager/AssignmentManagerApplication.java
@@ -0,0 +1,13 @@
+package org.althaf.assignmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/althaf/assignmentmanager/AssignmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.althaf.assignmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
